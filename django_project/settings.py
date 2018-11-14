a15b4365dfa855301e0bc0d028b1ea2d350d4ce9
alnagaev <alnagaev2014@gmail.com>
2018-11-14 19:23:09 +0300

    sys added

--- django_project/settings.py
+++ django_project/settings.py
@@ -8,7 +8,9 @@ https://docs.djangoproject.com/en/2.1/ref/settings/
 """
 
 import os
-from .settings_secret import *
+import sys
+sys.path.append("../django_project/django_project")
+from settings_secret import *
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
